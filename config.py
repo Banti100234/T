4caf0a74097b2a38d2220f6be31dfedde7227e5f
Banti100234 <kk1903@cse.jgec.ac.in>
2024-12-21 11:51:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @KaliaYtOwner
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7155834562:AAH0zD2zyt8mHsR7PV07mgkcNwmRf6HPays")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@@KSKYT")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("719670185")
+
+
+
+
+
+
+
+  
